1aa016060892bea2edea04d1661d3a6fbf792f1e
WILLIAM OLIVEIRA DE LAGOS <09100483@l1975036.portoalegre.pucrsnet.br>
2013-05-02 22:31:50 -0300

    Externalized GLUT and GLUI to library directory

---
+++ library/GLUI/CMakeFiles/glui.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/algebra3.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/algebra3.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/arcball.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/arcball.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_add_controls.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_add_controls.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_bitmap_img_data.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_bitmap_img_data.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_bitmaps.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_bitmaps.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_button.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_button.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_checkbox.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_checkbox.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_column.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_column.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_commandline.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_commandline.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_control.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_control.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_edittext.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_edittext.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_filebrowser.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_filebrowser.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_list.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_list.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_listbox.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_listbox.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_mouse_iaction.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_mouse_iaction.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_node.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_node.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_panel.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_panel.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_radio.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_radio.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_rollout.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_rollout.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_rotation.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_rotation.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_scrollbar.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_scrollbar.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_separator.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_separator.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_spinner.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_spinner.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_statictext.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_statictext.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_string.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_string.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_textbox.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_textbox.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_translation.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_translation.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_tree.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_tree.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_treepanel.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_treepanel.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/glui_window.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/glui_window.cpp.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/quaternion.cpp" "/media/09100483/EFFORIA/dinamo.hub/library/GLUI/CMakeFiles/glui.dir/quaternion.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "library/GLUI/.."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ library/GLUT/CMakeFiles/freeglut_static.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_callbacks.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_callbacks.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_cursor.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_cursor.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_display.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_display.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_ext.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_ext.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_font.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_font.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_font_data.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_font_data.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_gamemode.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_gamemode.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_geometry.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_geometry.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_glutfont_definitions.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_glutfont_definitions.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_init.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_init.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_input_devices.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_input_devices.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_joystick.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_joystick.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_main.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_main.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_menu.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_menu.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_misc.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_misc.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_overlay.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_overlay.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_spaceball.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_spaceball.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_state.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_state.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_stroke_mono_roman.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_stroke_mono_roman.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_stroke_roman.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_stroke_roman.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_structure.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_structure.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_teapot.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_teapot.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_videoresize.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_videoresize.c.o"
+  "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/freeglut_window.c" "/media/09100483/EFFORIA/dinamo.hub/library/GLUT/CMakeFiles/freeglut_static.dir/freeglut_window.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "FREEGLUT_EXPORTS"
+  "FREEGLUT_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  "__unix__"
+  "HAVE_FCNTL_H"
+  "HAVE_GETTIMEOFDAY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "library/GLUT/."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
